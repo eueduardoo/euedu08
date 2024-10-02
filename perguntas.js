107e51f0e640052f71955fa9f52f20b3c085bf3b
eueduardoo <160048424+eueduardoo@users.noreply.github.com>
2024-10-02 14:55:39 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,52 +1,62 @@
-criaCartao(
+criaCartao
+(
     'Historia',
     'Quem foi o responsável pela descoberta da América em 1492?',
     'Cristóvão Colombo, um explorador genovês a serviço da Espanha, foi o responsável pela descoberta da América em 1492, ao chegar às ilhas do Caribe, acreditando ter encontrado uma nova rota para as Índias.'
 )
 
-criaCartao(
+criaCartao
+(
     'Fisica',
     ' O que é a velocidade da luz no vácuo?',
     'A velocidade da luz no vácuo é aproximadamente 299.792.458 metros por segundo (m/s).'
 )
 
-criaCartao(
+criaCartao
+(
     'Matematica',
     'Qual é o valor de Pi (π) até a terceira casa decimal?',
     'O valor de Pi (π) até a terceira casa decimal é 3,141'
 )
 
-criaCartao(
+criaCartao
+(
     'Biologia',
     'Qual é a função dos glóbulos vermelhos no corpo humano?',
     ' Os glóbulos vermelhos têm a função de transportar oxigênio dos pulmões para os tecidos do corpo e trazer o dióxido de carbono dos tecidos de volta aos pulmões para ser expelido.'
 )
-criaCartao(
+criaCartao
+(
     'Quimica',
     ' Qual é a fórmula química da água?',
     'A fórmula química da água é H₂O, composta por dois átomos de hidrogênio e um átomo de oxigênio.'
 )
-criaCartao(
+criaCartao
+(
     'Geografia',
     'Qual é o maior continente em área territorial?',
     ' O maior continente em área territorial é a Ásia.' 
 )
-criaCartao(
+criaCartao
+(
     'Literatura',
     ' Quem escreveu o romance "Dom Quixote"?',
     ' O romance "Dom Quixote" foi escrito pelo autor espanhol Miguel de Cervantes' 
 )
-criaCartao(
+criaCartao
+(
     'Tecnologia',
     'O que é um algoritmo?',
     'Um algoritmo é uma sequência de instruções ou passos lógicos que devem ser seguidos para resolver um problema ou executar uma tarefa.'
 )
-criaCartao(
+criaCartao
+(
     'Astronomia',
     'Qual é o nome da maior lua de Júpiter?',
     'A maior lua de Júpiter é Ganimedes.' 
 )
-criaCartao(
+criaCartao
+(
     'Ecologia',
     'O que é biodiversidade?',
     'Biodiversidade é a variedade de formas de vida existentes em um ecossistema, abrangendo desde as espécies de plantas e animais até os micro-organismos' 
