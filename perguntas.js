a4dd2788977e2f69324ef36eeffcb4f08f003899
eueduardoo <160048424+eueduardoo@users.noreply.github.com>
2024-10-02 14:41:44 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,34 @@ criaCartao(
     'Qual é a função dos glóbulos vermelhos no corpo humano?',
     ' Os glóbulos vermelhos têm a função de transportar oxigênio dos pulmões para os tecidos do corpo e trazer o dióxido de carbono dos tecidos de volta aos pulmões para ser expelido.'
 )
+criaCartao(
+    'Quimica',
+    ' Qual é a fórmula química da água?',
+    'A fórmula química da água é H₂O, composta por dois átomos de hidrogênio e um átomo de oxigênio.'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente em área territorial?',
+    ' O maior continente em área territorial é a Ásia.' 
+)
+criaCartao(
+    'Literatura',
+    ' Quem escreveu o romance "Dom Quixote"?',
+    ' O romance "Dom Quixote" foi escrito pelo autor espanhol Miguel de Cervantes' 
+)
+    criaCartao(
+    'Tecnologia',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de instruções ou passos lógicos que devem ser seguidos para resolver um problema ou executar uma tarefa.
+'
+)
+criaCartao(
+    'Astronomia',
+    'Qual é o nome da maior lua de Júpiter?',
+    'A maior lua de Júpiter é Ganimedes.' 
+)
+criaCartao(
+    'Ecologia',
+    'O que é biodiversidade?',
+    'Biodiversidade é a variedade de formas de vida existentes em um ecossistema, abrangendo desde as espécies de plantas e animais até os micro-organismos' 
+)
