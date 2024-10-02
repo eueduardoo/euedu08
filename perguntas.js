3c066ae0e028a94835a7fb0f6d0f198b23f2a75d
eueduardoo <160048424+eueduardoo@users.noreply.github.com>
2024-10-02 14:25:45 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Historia',
+    'Quem foi o responsável pela descoberta da América em 1492?',
+    'Cristóvão Colombo, um explorador genovês a serviço da Espanha, foi o responsável pela descoberta da América em 1492, ao chegar às ilhas do Caribe, acreditando ter encontrado uma nova rota para as Índias.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Fisica',
+    ' O que é a velocidade da luz no vácuo?',
+    'A velocidade da luz no vácuo é aproximadamente 299.792.458 metros por segundo (m/s).'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matematica',
+    'Qual é o valor de Pi (π) até a terceira casa decimal?',
+    'O valor de Pi (π) até a terceira casa decimal é 3,141'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Biologia',
+    'Qual é a função dos glóbulos vermelhos no corpo humano?',
+    ' Os glóbulos vermelhos têm a função de transportar oxigênio dos pulmões para os tecidos do corpo e trazer o dióxido de carbono dos tecidos de volta aos pulmões para ser expelido.'
 )
